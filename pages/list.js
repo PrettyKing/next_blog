688175084fac17532d6c76553b9eec1d7f3e7cc6
chalee <chalee@chalee-2.lan>
2020-03-14 16:54:34 +0800

    aa

--- pages/list.js
+++ pages/list.js
@@ -1,7 +1,7 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-import  servicePath  from '../config/apiUrl'
-import Link from 'next/link'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import servicePath from "../config/apiUrl";
+import Link from "next/link";
 import Head from "next/head";
 import { Row, Col, List, Icon, Breadcrumb } from "antd";
 import Header from "../components/Header";
@@ -10,11 +10,11 @@ import Advert from "../components/Advert";
 import Footer from "../components/Footer";
 // import "../static/style/pages/list.css";
 
-const ArticleList = (list) => {
-  const [ mylist , setMylist ] = useState(list.data);
-  useEffect(()=>{
-    setMylist(list.data)
-  })
+const ArticleList = list => {
+  const [mylist, setMylist] = useState(list.data);
+  useEffect(() => {
+    setMylist(list.data);
+  });
   return (
     <>
       <Head>
@@ -39,41 +39,45 @@ const ArticleList = (list) => {
               renderItem={item => (
                 <List.Item>
                   <div className="list-title">
-                      <Link href={{pathname:'/detailed',query:{id:item.id}}}>
+                    <Link
+                      href={{ pathname: "/detailed", query: { id: item.id } }}
+                    >
                       <a>{item.title}</a>
                     </Link>
                   </div>
                   <div className="list-icon">
-                    <span><Icon type="calendar" />{item.addTime}</span>
-                    <span><Icon type="folder" /> {item.typeName}</span>
-                    <span><Icon type="fire" />  {item.view_count}人</span>
+                    <span>
+                      <Icon type="calendar" />
+                      {item.addTime}
+                    </span>
+                    <span>
+                      <Icon type="folder" /> {item.typeName}
+                    </span>
+                    <span>
+                      <Icon type="fire" /> {item.view_count}人
+                    </span>
                   </div>
-                  <div className="list-context">{item.introduce}</div>  
+                  <div className="list-context">{item.introduce}</div>
                 </List.Item>
               )}
-            />  
+            />
           </div>
         </Col>
 
         <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
-          <Advert />
         </Col>
       </Row>
       <Footer />
     </>
   );
 };
-ArticleList.getInitialProps = async (context)=>{
-
-  let id =context.query.id
-  const promise = new Promise((resolve)=>{
-    axios(servicePath.getListById+id).then(
-      (res)=>resolve(res.data)
-    )
-  })
-  return await promise
-}
-
+ArticleList.getInitialProps = async context => {
+  let id = context.query.id;
+  const promise = new Promise(resolve => {
+    axios(servicePath.getListById + id).then(res => resolve(res.data));
+  });
+  return await promise;
+};
 
 export default ArticleList;
